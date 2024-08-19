ab7c4b4b1554ee2c633200487d0b7bb2f029e6b3
Marc Wimmer <marc@zebroo.de>
2024-08-19 22:06:51 +0200

    manifest robotutils

--- robot_utils/__manifest__.py
+++ robot_utils/__manifest__.py
@@ -1,16 +1,11 @@
-
 {
-    'name': 'robot_utils',
-    'version': '13.0.1.0',
+    "name": "robot_utils",
+    "version": "1.0",
     "auto_install": False,
-    'author': 'Marc Wimmer (marc@itewimmer.de)',
-    'depends': ['base'],
-    'data': [],
+    "author": "Marc Wimmer (marc@itewimmer.de)",
+    "depends": ["base"],
+    "data": [],
     "license": "Other proprietary",
     "external_dependencies": {"python": [], "bin": []},
     "license": "Other proprietary",
-    
-    #"uninstall_hook": "uninstall_hook",
-    #'pre_init_hook': 'pre_init_hook',
-    #'post_init_hook': 'post_init_hook',
 }
