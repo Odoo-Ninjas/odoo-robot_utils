863f114cae700f363d57700460ddcada2579ea38
Marc Wimmer <marc@zebroo.de>
2025-01-06 20:35:01 +0100

    handling checkboxes

--- robot_utils/keywords/js/inputelement.js
+++ robot_utils/keywords/js/inputelement.js
@@ -2,7 +2,14 @@ const callback = arguments[arguments.length - 1];
 const result = css + " " + value;
 const el =  document.querySelector(css);
 el.focus();
-el.value = value;
+
+if (el.type === 'checkbox') {
+	let boolvalue = value === 'true' || value === '1' || value === true || value == "TRUE" || value == "True";
+	el.checked = boolvalue;
+}
+else {
+	el.value = value;
+}
 
 // needed to activate save button
 const event = new Event("input", { bubbles: true });
