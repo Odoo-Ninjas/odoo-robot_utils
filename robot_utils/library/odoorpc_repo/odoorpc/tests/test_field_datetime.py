db8cdf14a80fca99c894838fbfd9bbe59079b310
Marc Wimmer <marc@zebroo.de>
2025-02-23 12:25:11 +0100

    Updating submodule robot_utils/library/odoorpc_repo

---
+++ robot_utils/library/odoorpc_repo/odoorpc/tests/test_field_datetime.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from odoorpc.tests import LoginTestCase
+
+
+class TestFieldDatetime(LoginTestCase):
+    def test_field_datetime_read(self):
+        SaleOrder = self.odoo.env['sale.order']
+        order_id = SaleOrder.search([('date_order', '!=', False)], limit=1)
+        order = SaleOrder.browse(order_id)
+        self.assertIsInstance(order.date_order, datetime.datetime)
+
+    def test_field_datetime_write(self):
+        # TODO
+        # No common model found in every versions of OpenERP with a
+        # fields.datetime writable
+        pass
